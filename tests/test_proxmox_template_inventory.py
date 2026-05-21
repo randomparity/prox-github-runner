1e6525ec9fd2dad874a3eba2c1d6d2d65dc6582b
David Christensen <randomparity@gmail.com>
2026-05-20 17:39:12 -0700

    Fix Ubuntu image inventory policy test

--- tests/test_proxmox_template_inventory.py
+++ tests/test_proxmox_template_inventory.py
@@ -9,11 +9,15 @@ def load_proxmox_vars() -> dict[str, object]:
     return yaml.safe_load(Path("inventory/group_vars/proxmox/vars.yml").read_text())
 
 
-def test_ubuntu_cloud_image_uses_current_url() -> None:
+def test_ubuntu_cloud_image_uses_current_url_with_pinned_checksum() -> None:
     data = load_proxmox_vars()
-    assert data["proxmox_template_cloud_image_url"] == (
-        "https://cloud-images.ubuntu.com/noble/current/"
-        "noble-server-cloudimg-amd64.img"
+    assert (
+        data["proxmox_template_cloud_image_url"]
+        == "https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img"
+    )
+    assert (
+        data["proxmox_template_cloud_image_checksum"]
+        == "sha256:6e7016f2c9f4d3c00f48789eb6b9043ba2172ccc1b6b1eaf3ed1e29dd3e52bb3"
     )
 
 
